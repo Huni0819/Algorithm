48391cc2a062753508cf726a4efdb02999e761b0
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-04-03 10:36:00 +0900

    [Silver V] Title: 숫자 카드, Time: 984 ms, Memory: 137836 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -2,27 +2,34 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
 
     private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     private static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    private static StringBuilder builder = new StringBuilder();
-    private static int[] cardArray;
-    private static int[] numArray;
+    private static Set<Integer> card = new HashSet<>();
 
     public static void main(String[] args) throws Exception {
 
-        cardArray = input();
-        numArray = input();
+        int n = Integer.valueOf(reader.readLine());
 
-        Arrays.sort(cardArray);
+        StringTokenizer st = new StringTokenizer(reader.readLine());
 
-        for (int num : numArray) {
-            builder.append(solution(num, 0, cardArray.length - 1) ? "1 " : "0 ");
+        for (int i = 0; i < n; i++) {
+            card.add(Integer.valueOf(st.nextToken()));
+        }
+
+        int m = Integer.valueOf(reader.readLine());
+
+        StringBuilder builder = new StringBuilder();
+        st = new StringTokenizer(reader.readLine());
+        for (int i = 0; i < m; i++) {
+            int check = card.contains(Integer.valueOf(st.nextToken())) ? 1 : 0;
+
+            builder.append(check).append(" ");
         }
 
         writer.write(builder.toString().trim());
@@ -32,34 +39,4 @@ public class Main {
         writer.close();
     }
 
-    private static int[] input() throws Exception {
-        int n = Integer.valueOf(reader.readLine());
-
-        int[] array = new int[n];
-
-        StringTokenizer st = new StringTokenizer(reader.readLine());
-
-        for(int i = 0; i < n; i++) {
-            array[i] = Integer.valueOf(st.nextToken());
-        }
-
-        return array;
-    }
-
-    private static boolean solution(int num, int low, int high) {
-
-
-        while (low <= high) {
-            int middle = (low + high) / 2;
-
-            if (cardArray[middle] == num) {
-                return true;
-            }
-
-            low = cardArray[middle] < num ? middle + 1 : low;
-            high = cardArray[middle] > num ? middle - 1 : high;
-        }
-
-        return false;
-    }
 }
