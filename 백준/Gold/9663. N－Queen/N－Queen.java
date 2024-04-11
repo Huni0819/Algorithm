a40457b0e6c5605fdc49d0393496bc09ef81a432
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-04-11 11:39:49 +0900

    [Gold IV] Title: N-Queen, Time: 5288 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -3,57 +3,51 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static boolean[][] visit;
-
-    static int result = 0;
-
+    static int[] array;
     static int n;
+    static int count = 0;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(reader.readLine());
 
-        visit = new boolean[n][n];
+        array = new int[n];
+
         solution(0);
 
-        System.out.println(result);
+        System.out.println(count);
 
         reader.close();
     }
 
-    private static void solution(int x) {
-        if (x == n) {
-            result++;
+    private static void solution(int depth) {
+        if (depth == n) {
+            count++;
             return;
         }
 
         for (int i = 0; i < n; i++) {
-            if (check(x, i)) {
-                visit[x][i] = true;
-                solution(x+1);
-                visit[x][i] = false;
+            array[depth] = i;
+
+            if (check(depth)) {
+                solution(depth+1);
             }
         }
     }
 
-    private static boolean check(int x, int y) {
-        for (int i = 0; i < n; i++) {
-            if ((i <= y && visit[x][i]) || (i <= x && visit[i][y])) {
+    private static boolean check(int depth) {
+        for (int i = 0; i < depth; i++) {
+            if (array[depth] == array[i]) {
                 return false;
-            }
-
-            if ((x+i < n && y+i < n && visit[x+i][y+i]) || (x+i < n && y-i >= 0 && visit[x+i][y-i])) {
-                return false;
-            }
-
-            if ((x-i >= 0 && y-i >= 0 && visit[x-i][y-i]) || (x-i >= 0 && y+i <n && visit[x-i][y+i])) {
+            } else if (Math.abs(depth-i) == Math.abs(array[depth] - array[i])) {
                 return false;
             }
         }
 
         return true;
     }
-
 
 }
