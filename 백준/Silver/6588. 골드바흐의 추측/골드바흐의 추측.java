893b6cf23ae47c50440c934500b628f57c970b82
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-04-15 12:19:18 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 1500 ms, Memory: 54940 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    static boolean[] isPrime = new boolean[1_000_001];
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        Arrays.fill(isPrime, true);
+
+        calcPrime();
+
+        String line;
+        while (!(line = reader.readLine()).equals("0")) {
+
+            solution(Integer.parseInt(line));
+        }
+
+        reader.close();
+    }
+
+    private static void calcPrime() {
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for (int i = 2; i <= Math.sqrt(1_000_000); i++) {
+            for (int j = i * 2; j < 1_000_001; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    private static void solution(int num) {
+        StringBuilder builder = new StringBuilder();
+        boolean flag = true;
+
+        for (int i = 2; i <= num / 2; i++) {
+            if (isPrime[i] && isPrime[num - i]) {
+                builder.append(num)
+                        .append(" = ")
+                        .append(i)
+                        .append(" + ")
+                        .append(num - i);
+                System.out.println(builder);
+                flag = false;
+                return;
+            }
+        }
+
+        if (flag) {
+            System.out.println("Goldbach's conjecture is wrong.");
+        }
+    }
+
+}
