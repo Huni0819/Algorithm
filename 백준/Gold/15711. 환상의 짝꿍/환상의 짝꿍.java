c799b5aa0c8aae03d088b4a999914207887170f0
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-04-15 13:18:11 +0900

    [Gold III] Title: 환상의 짝꿍, Time: 2072 ms, Memory: 16880 KB -BaekjoonHub

---
+++ 백준/Gold/15711. 환상의 짝꿍/환상의 짝꿍.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static boolean[] isPrime;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(reader.readLine());
+
+        isPrime = new boolean[(int) (Math.pow(10, 6) * 2 + 1)];
+        Arrays.fill(isPrime, true);
+        primeCalc();
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(reader.readLine());
+            long a = Long.parseLong(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+
+            solution(a+b);
+
+        }
+
+        reader.close();
+    }
+
+    private static void primeCalc() {
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for (int i = 2; i <= Math.sqrt(isPrime.length); i++) {
+
+            for (int j = i * 2; j < isPrime.length; j+=i) {
+                if (isPrime[j]) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+    }
+
+    private static void solution(long num) {
+        if (num == 2) {
+            System.out.println("NO");
+        } else if (num % 2 == 0) {
+            System.out.println("YES"); 
+        } else {
+            if (isPrime(num -2)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+
+    }
+
+    private static boolean isPrime(long num) {
+        if (num <= isPrime.length) {
+            return isPrime[(int) num];
+        } else {
+            for (int i = 0; i < isPrime.length; i++) {
+                if (isPrime[i]) {
+                    if (num % i == 0) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
