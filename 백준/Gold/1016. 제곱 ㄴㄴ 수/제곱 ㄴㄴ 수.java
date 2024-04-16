7d8e007d10ff51e88d7f3edf2cb79b6bb5689480
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-04-16 13:52:49 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 164 ms, Memory: 15400 KB -BaekjoonHub

---
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+
+        long min = Long.parseLong(st.nextToken());
+        long max = Long.parseLong(st.nextToken());
+
+        solution(min, max);
+
+        reader.close();
+    }
+
+    public static void solution(long min, long max) {
+
+        int size = (int) (max - min);
+        boolean[] arr = new boolean[size + 1];
+
+        for (long i = 2; i <= Math.sqrt(max); i++) {
+            long pow = i * i;
+
+            long tmp = min / pow;
+
+            if (min % pow != 0) {
+                tmp++;
+            }
+
+            for (long j = tmp; j * pow <= max; j++) {
+                arr[(int) (j * pow - min)] = true;
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i <= size; i++) {
+            if (!arr[i]) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
