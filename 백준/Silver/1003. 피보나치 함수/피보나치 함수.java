d5dfb739252eebae8b3eea2a557653e94d6a16a3
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-04-17 14:59:45 +0900

    [Silver III] Title: 피보나치 함수, Time: 120 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,65 +1,47 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int count = Integer.parseInt(br.readLine());
+    static StringBuilder builder = new StringBuilder();
+    static int[] zeroArr;
+    static int[] oneArr;
 
-        for (int i = 0; i < count; i++) {
-            int n = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws Exception {
 
-            Count c = new Count();
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
 
-            fibonacci(n, c);
+        int t = Integer.parseInt(reader.readLine());
 
-            System.out.println(c.getZeroCount() + " " + c.getOneCount());
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(reader.readLine());
+            zeroArr = new int[n+1];
+            oneArr = new int[n+1];
+            solution(n);
         }
+
+        System.out.println(builder);
+        reader.close();
     }
 
-    private static void fibonacci(int n, Count count) {
-        for (int i = 0; i < n; i++) {
-            count.setZeroCount(count.getOneCount());
-            count.setOneCount(count.getZeroPlusOne());
-            count.setZeroPlusOne(count.getZeroCount() + count.getOneCount());
-        }
-    }
-
-    private static class Count {
-        int zeroCount;
-        int oneCount;
-        int zeroPlusOne;
-
-        public Count() {
-            zeroCount = 1;
-            oneCount = 0;
-            zeroPlusOne = 1;
+    private static void solution(int num) {
+        if (num == 0) {
+            builder.append(1).append(" ").append(0).append("\n");
+            return;
+        } else if (num == 1) {
+            builder.append(0).append(" ").append(1).append("\n");
+            return;
         }
 
-        public int getZeroCount() {
-            return zeroCount;
+        zeroArr[0] = oneArr[1] = 1;
+        zeroArr[1] = oneArr[0] = 0;
+
+        for (int i = 2; i <= num; i++) {
+            zeroArr[i] = zeroArr[i-1] + zeroArr[i-2];
+            oneArr[i] = oneArr[i-1] + oneArr[i-2];
         }
 
-        public void setZeroCount(int zeroCount) {
-            this.zeroCount = zeroCount;
-        }
-
-        public int getOneCount() {
-            return oneCount;
-        }
-
-        public void setOneCount(int oneCount) {
-            this.oneCount = oneCount;
-        }
-
-        public int getZeroPlusOne() {
-            return zeroPlusOne;
-        }
-
-        public void setZeroPlusOne(int zeroPlusOne) {
-            this.zeroPlusOne = zeroPlusOne;
-        }
+        builder.append(zeroArr[num]).append(" ").append(oneArr[num]).append("\n");
     }
 }
