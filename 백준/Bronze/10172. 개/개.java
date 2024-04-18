3e5ed1af9d3e8fe1a11bdd5ebb0aab43e8da6b1a
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-04-18 17:05:52 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
