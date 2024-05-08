f9a79aa4a865dd6eb9886d5a5cd022e0a48121a7
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-05-08 12:42:20 +0900

    [Silver V] Title: 올림픽, Time: 144 ms, Memory: 14664 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -1,47 +1,56 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(reader.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int index = 0;
 
-        int arr[][] = new int[N][4];
+        int[][] arr = new int[n][4];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(reader.readLine());
 
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
             arr[i][2] = Integer.parseInt(st.nextToken());
             arr[i][3] = Integer.parseInt(st.nextToken());
+
+            if (arr[i][0] == k) {
+                index = i;
+            }
         }
 
-        Arrays.sort(arr, (o1, o2) -> {
-            if (o1[1] == o2[1]) {
-                if (o1[2] == o2[2]) {
-                    return o2[3] - o1[3];
+        int result = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (i != index) {
+
+                if (arr[i][1] > arr[index][1]) {
+                    result++;
+                } else if (arr[i][1] == arr[index][1]) {
+
+                    if (arr[i][2] > arr[index][2]) {
+                        result++;
+                    } else if (arr[i][2] == arr[index][2]) {
+
+                        if (arr[i][3] > arr[index][3]) {
+                            result++;
+                        }
+                    }
                 }
-
-                return o2[2] - o1[2];
-            }
-
-            return o2[1] - o1[1];
-        });
-
-        for (int i = 0; i < N; i++) {
-            if (arr[i][0] == K) {
-                System.out.println(i);
-                break;
             }
         }
+
+        System.out.println(result);
 
         reader.close();
     }
