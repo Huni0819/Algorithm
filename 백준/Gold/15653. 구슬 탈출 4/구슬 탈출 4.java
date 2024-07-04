013cb3847841a51e0b39172ab2981d22d2add33e
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-07-04 15:33:27 +0900

    [Gold I] Title: 구슬 탈출 4, Time: 108 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Gold/15653. 구슬 탈출 4/구슬 탈출 4.java
@@ -0,0 +1,197 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    static int N;
+    static int M;
+    static char[][] arr;
+
+    static boolean[][][][] visited;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = reader.readLine().split(" ");
+
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+
+        arr = new char[N][M];
+        visited = new boolean[N][M][N][M];
+
+        Point init = new Point();
+        for (int i = 0; i < N; i++) {
+            String line = reader.readLine();
+
+            for (int j = 0; j < M; j++) {
+
+                arr[i][j] = line.charAt(j);
+                if (line.charAt(j) == 'R') {
+                    init.rx = i;
+                    init.ry = j;
+
+                    arr[i][j] = '.';
+                }
+
+                if (line.charAt(j) == 'B') {
+                    init.bx = i;
+                    init.by = j;
+
+                    arr[i][j] = '.';
+                }
+            }
+        }
+
+        System.out.println(solution(init));
+    }
+
+    private static int solution(Point init) {
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(init);
+
+        while (!queue.isEmpty()) {
+            Point now = queue.poll();
+            visited[now.rx][now.ry][now.bx][now.by] = true;
+
+            for (int i = 0; i < 4; i++) {
+
+                int rx = now.rx;
+                int ry = now.ry;
+                int bx = now.bx;
+                int by = now.by;
+
+                boolean isR = false;
+                boolean isB = false;
+
+                while (true) {
+
+                    rx += dx[i];
+                    ry += dy[i];
+
+                    if (checkBound(rx, ry)) {
+                        rx -= dx[i];
+                        ry -= dy[i];
+                        break;
+                    }
+
+                    if (arr[rx][ry] == '#') {
+                        rx -= dx[i];
+                        ry -= dy[i];
+                        break;
+                    }
+
+                    if (arr[rx][ry] == 'O') {
+                        isR = true;
+                        break;
+                    }
+                }
+
+                while (true) {
+
+                    bx += dx[i];
+                    by += dy[i];
+
+                    if (checkBound(bx, by)) {
+                        bx -= dx[i];
+                        by -= dy[i];
+                        break;
+                    }
+
+                    if (arr[bx][by] == '#') {
+                        bx -= dx[i];
+                        by -= dy[i];
+                        break;
+                    }
+
+                    if (arr[bx][by] == 'O') {
+                        isB = true;
+                        break;
+                    }
+                }
+
+                if (isB) {
+                    continue;
+                }
+
+                if (isR) {
+                    return now.count + 1;
+                }
+
+                if (rx == bx && ry == by) {
+
+                    switch (i) {
+                        // 0 -> {-1, 0} | 1 -> {1, 0} | 2 -> {0, -1} | 3 -> {0, 1}
+                        case 0:
+                            if (now.rx > now.bx) {
+                                rx -= dx[i];
+                            } else {
+                                bx -= dx[i];
+                            }
+                            break;
+                        case 1:
+                            if (now.rx > now.bx) {
+                                bx -= dx[i];
+                            } else {
+                                rx -= dx[i];
+                            }
+                            break;
+                        case 2:
+                            if (now.ry > now.by) {
+                                ry -= dy[i];
+                            } else {
+                                by -= dy[i];
+                            }
+                            break;
+                        case 3:
+                            if (now.ry > now.by) {
+                                by -= dy[i];
+                            } else {
+                                ry -= dy[i];
+                            }
+                            break;
+                    }
+                }
+
+                if (!visited[rx][ry][bx][by]) {
+                    queue.offer(new Point(rx, ry, bx, by, now.count + 1));
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    private static boolean checkBound(int x, int y) {
+        return x < 0 || y < 0 || x >= N || y >= M;
+    }
+
+
+}
+
+
+class Point {
+
+    int rx;
+    int ry;
+    int bx;
+    int by;
+    int count;
+
+    public Point() {
+    }
+
+    public Point(int rx, int ry, int bx, int by, int count) {
+        this.rx = rx;
+        this.ry = ry;
+        this.bx = bx;
+        this.by = by;
+        this.count = count;
+    }
+}
