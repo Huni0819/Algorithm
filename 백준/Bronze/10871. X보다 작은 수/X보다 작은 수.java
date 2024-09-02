53db5cd6b779b3ece524d7cbc07f9ea67d20ba83
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-09-02 14:12:52 +0900

    [Bronze V] Title: X보다 작은 수, Time: 132 ms, Memory: 15844 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,30 +1,26 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        BufferedReader reader =
-                new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(reader.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int x = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
+        String[] input = reader.readLine().split(" ");
 
-        st = new StringTokenizer(reader.readLine());
+        int N = Integer.parseInt(input[0]);
+        int X = Integer.parseInt(input[1]);
+
         StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(st.nextToken());
 
-            if (num < x) {
-                builder.append(num).append(" ");
+        input = reader.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            if (Integer.parseInt(input[i]) < X) {
+                builder.append(input[i]).append(" ");
             }
         }
 
         System.out.println(builder);
-        reader.close();
     }
 }
