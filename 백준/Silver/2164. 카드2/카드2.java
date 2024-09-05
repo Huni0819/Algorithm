5cabb000b3a3f734993f835a750e6ed53758510b
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-09-05 16:32:49 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 45492 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(reader.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.offer(i);
+        }
+
+        while (true) {
+            if (queue.size() == 1) {
+                System.out.println(queue.poll());
+                break;
+            }
+
+            queue.poll();
+            queue.offer(queue.poll());
+        }
+    }
+}
