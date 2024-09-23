f19037b26c26ee3b22e123a0f67054f4089f8620
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-09-23 13:20:30 +0900

    [Silver I] Title: 그림, Time: 372 ms, Memory: 49888 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -2,15 +2,18 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
 
+    static Coord[] dCoord = {
+            new Coord(1, 0),
+            new Coord(-1, 0),
+            new Coord(0, 1),
+            new Coord(0, -1)
+    };
+
     static int n;
     static int m;
-
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
 
     static int[][] arr;
     static boolean[][] visited;
@@ -19,25 +22,26 @@ public class Main {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(reader.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        String[] input = reader.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
 
         arr = new int[n][m];
         visited = new boolean[n][m];
-
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(reader.readLine());
+            input = reader.readLine().split(" ");
 
             for (int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(input[j]);
             }
         }
 
         int max = 0;
         int count = 0;
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
+
                 if (arr[i][j] == 1 && !visited[i][j]) {
                     count++;
                     max = Math.max(max, solution(i, j));
@@ -48,27 +52,30 @@ public class Main {
         System.out.println(count);
         System.out.println(max);
 
-        reader.close();
     }
 
     private static int solution(int x, int y) {
-        Queue<Coordinate> queue = new LinkedList<>();
-        queue.offer(new Coordinate(x, y));
+        Queue<Coord> queue = new LinkedList<>();
+        queue.offer(new Coord(x, y));
         visited[x][y] = true;
 
         int count = 1;
 
         while (!queue.isEmpty()) {
-            Coordinate now = queue.poll();
+            Coord now = queue.poll();
 
             for (int i = 0; i < 4; i++) {
-                int nx = now.getX() + dx[i];
-                int ny = now.getY() + dy[i];
+                int nx = now.x + dCoord[i].x;
+                int ny = now.y + dCoord[i].y;
 
-                if (nx >= 0 && ny >= 0 && nx < n && ny < m && arr[nx][ny] == 1 && !visited[nx][ny]) {
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                    continue;
+                }
+
+                if (arr[nx][ny] == 1 && !visited[nx][ny]) {
                     count++;
+                    queue.offer(new Coord(nx, ny));
                     visited[nx][ny] = true;
-                    queue.offer(new Coordinate(nx, ny));
                 }
             }
         }
@@ -77,21 +84,13 @@ public class Main {
     }
 }
 
-class Coordinate {
+class Coord {
 
-    private int x;
-    private int y;
+    int x;
+    int y;
 
-    public Coordinate(int x, int y) {
+    public Coord(int x, int y) {
         this.x = x;
         this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
     }
 }
