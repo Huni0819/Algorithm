e7a7e9a75c9c04c2600c3aa7204388ecf7b041ac
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-09-26 13:26:46 +0900

    [Gold IV] Title: 불, Time: 540 ms, Memory: 63620 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -2,19 +2,21 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static int[][] d = {
+            {1, 0}, {-1, 0}, {0, 1}, {0, -1}
+    };
 
-    static Deque<Coordinate> deque;
+    static int W;
+    static int H;
 
-    static int[][] visited;
     static char[][] arr;
-    static int w;
-    static int h;
+    static int[][] visited;
+    static Deque<Coord> deque = new ArrayDeque<>();
+
+    static StringBuilder builder = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
 
@@ -23,98 +25,77 @@ public class Main {
         int T = Integer.parseInt(reader.readLine());
 
         for (int tc = 0; tc < T; tc++) {
+            deque.clear();
 
-            StringTokenizer st = new StringTokenizer(reader.readLine());
-            w = Integer.parseInt(st.nextToken());
-            h = Integer.parseInt(st.nextToken());
+            String[] input = reader.readLine().split(" ");
+            W = Integer.parseInt(input[0]);
+            H = Integer.parseInt(input[1]);
 
-            deque = new ArrayDeque<>();
-            arr = new char[h][w];
-            visited = new int[h][w];
+            arr = new char[H][W];
+            visited = new int[H][W];
 
-            for (int i = 0; i < h; i++) {
+            for (int i = 0; i < H; i++) {
                 String line = reader.readLine();
-                for (int j = 0; j < w; j++) {
+                for (int j = 0; j < W; j++) {
                     arr[i][j] = line.charAt(j);
 
-                    if (line.charAt(j) == '@') {
-                        deque.offerFirst(new Coordinate(i, j, true));
-                        visited[i][j] = 1;
+                    if (arr[i][j] == '@') {
                         arr[i][j] = '.';
-                    } else if (line.charAt(j) == '*') {
-                        deque.offerLast(new Coordinate(i, j, false));
+                        deque.offerLast(new Coord(i, j, true));
+                        visited[i][j] = 1;
+                    } else if (arr[i][j] == '*') {
+                        deque.offerFirst(new Coord(i, j, false));
                     }
                 }
             }
 
-            int count = solution();
-
-            System.out.println(count == -1 ? "IMPOSSIBLE" : count);
+            solution();
         }
 
-        reader.close();
+        System.out.println(builder);
     }
 
-    private static int solution() {
+    static void solution() {
 
         while (!deque.isEmpty()) {
-            Coordinate now = deque.pollFirst();
+            Coord now = deque.poll();
 
-            if (now.isPerson() && arr[now.getX()][now.getY()] == '*') {
-                continue;
-            }
+            for (int i = 0; i < d.length; i++) {
+                int nx = now.x + d[i][0];
+                int ny = now.y + d[i][1];
 
-            for(int i = 0; i < 4; i++) {
-                int nx = now.getX() + dx[i];
-                int ny = now.getY() + dy[i];
-
-                if (nx < 0 || nx >= h || ny < 0 || ny >= w) {
-                    if (now.isPerson()) {
-                        return visited[now.getX()][now.getY()];
+                if (nx < 0 || nx >= H || ny < 0 || ny >= W) {
+                    if (now.isSang) {
+                        builder.append(visited[now.x][now.y])
+                                .append("\n");
+                        return;
                     }
+
                     continue;
                 }
 
-                if (arr[nx][ny] == '#') {
-                    continue;
-                }
+                if (arr[nx][ny] == '.' && visited[nx][ny] == 0) {
 
-                if (now.isPerson() && arr[nx][ny] == '.' && visited[nx][ny] == 0) {
-                    deque.offer(new Coordinate(nx, ny, true));
-                    visited[nx][ny] = visited[now.getX()][now.getY()] + 1;
-                } else if (!now.isPerson() && arr[nx][ny] == '.') {
-                    deque.offerLast(new Coordinate(nx, ny, false));
-                    arr[nx][ny] = '*';
+                    visited[nx][ny] = visited[now.x][now.y] + 1;
+                    deque.offer(new Coord(nx, ny, now.isSang));
                 }
             }
         }
 
-        return -1;
+        builder.append("IMPOSSIBLE")
+                .append("\n");
     }
 }
 
-class Coordinate {
+class Coord {
 
-    private int x;
-    private int y;
-    private boolean person;
+    int x;
+    int y;
+    boolean isSang;
 
-    public Coordinate(int x, int y, boolean person) {
+    public Coord(int x, int y, boolean isSang) {
         this.x = x;
         this.y = y;
-        this.person = person;
+        this.isSang = isSang;
     }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public boolean isPerson() {
-        return person;
-    }
-
 }
