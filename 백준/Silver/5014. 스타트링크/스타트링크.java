f9ae1128733af8701161c0f48e9b2cf8d63bea99
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-09-27 11:02:25 +0900

    [Silver I] Title: 스타트링크, Time: 184 ms, Memory: 61344 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.java
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -2,60 +2,51 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
-
-    static int f; // 총 건물 층 수
-    static int s; // 강호의 현재 위치
-    static int g; // 강호가 도착해야하는 층 수
-    static int u; // 한번에 올라가는 층 수
-    static int d; // 한번에 내려가는 층 수
-    static int[] visited; // 각 층 방문 여부
-
 
     public static void main(String[] args) throws Exception {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(reader.readLine());
-        f = Integer.parseInt(st.nextToken());
-        s = Integer.parseInt(st.nextToken());
-        g = Integer.parseInt(st.nextToken());
-        u = Integer.parseInt(st.nextToken());
-        d = Integer.parseInt(st.nextToken());
+        String[] input = reader.readLine().split(" ");
+        int F = Integer.parseInt(input[0]);
+        int S = Integer.parseInt(input[1]);
+        int G = Integer.parseInt(input[2]);
+        int U = Integer.parseInt(input[3]);
+        int D = Integer.parseInt(input[4]);
 
-        visited = new int[f+1];
+        int[] arr = new int[F+1];
+        int[] visited = new int[F+1];
 
-        solution();
-
-        reader.close();
-    }
-
-    private static void solution() {
         Queue<Integer> queue = new LinkedList<>();
-        queue.offer(s);
-        visited[s] = 1;
+        queue.offer(S);
+        visited[S] = 1;
 
-        while (!queue.isEmpty()) {
+        while (!queue.isEmpty()){
             int now = queue.poll();
 
-            if (now == g) {
-                System.out.println(visited[now]-1);
-                System.exit(0);
+            if (now == G) {
+                System.out.println(visited[now] - 1);
+                return;
             }
 
-            if (now + u <= f && visited[now+u] == 0) {
-                queue.offer(now+u);
-                visited[now+u] = visited[now] + 1;
+            if (now + U <= F) {
+                if (visited[now + U] == 0) {
+                    queue.offer(now + U);
+                    visited[now + U] = visited[now] + 1;
+                }
             }
 
-            if (now - d >= 1 && visited[now-d] == 0) {
-                queue.offer(now-d);
-                visited[now-d] = visited[now] + 1;
+            if (now - D >= 1) {
+                if (visited[now - D] == 0) {
+                    queue.offer(now - D);
+                    visited[now - D] = visited[now] + 1;
+                }
             }
         }
 
         System.out.println("use the stairs");
+
     }
 }
