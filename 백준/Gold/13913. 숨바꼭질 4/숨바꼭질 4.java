9c66c8079a79af0be772a1a70caf7bda10593dee
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-09-30 14:20:44 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 224 ms, Memory: 26112 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -1,68 +1,67 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder builder = new StringBuilder();
 
-        StringTokenizer st = new StringTokenizer(reader.readLine());
+        String[] input = reader.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        int[] arr = new int[100_001];
-        int[] prev = new int[100_001];
+        int[][] arr = new int[100_001][2]; // arr[0] -> 시간, arr[1] -> 이전 위치
 
         Queue<Integer> queue = new LinkedList<>();
-        queue.offer(n);
-        arr[n] = 1;
+        queue.offer(N);
+        arr[N][0] = 1;
 
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             int now = queue.poll();
 
-            if (now == m) {
-                System.out.println(arr[now]-1);
+            if (now == K) {
+                builder.append(arr[now][0] - 1)
+                        .append("\n");
+
+                int[] trace = new int[arr[now][0]];
+
+                int num = now;
+                for (int i = trace.length - 1; i >= 0; i--) {
+
+                    trace[i] = num;
+                    num = arr[num][1];
+                }
+
+                Arrays.stream(trace)
+                        .forEach(x -> builder.append(x).append(" "));
+
                 break;
             }
 
-            if (now - 1 >= 0 && arr[now-1] == 0) {
-                queue.offer(now-1);
-                arr[now-1] = arr[now] + 1;
-                prev[now-1] = now;
+            if (now + 1 <= 100_000 && arr[now + 1][0] == 0) {
+                queue.offer(now + 1);
+                arr[now + 1][0] = arr[now][0] + 1;
+                arr[now + 1][1] = now;
             }
 
-            if (now + 1 <= 100_000 && arr[now+1] == 0) {
-                queue.offer(now+1);
-                arr[now+1] = arr[now] + 1;
-                prev[now+1] = now;
+            if (now - 1 >= 0 && arr[now - 1][0] == 0) {
+                queue.offer(now - 1);
+                arr[now - 1][0] = arr[now][0] + 1;
+                arr[now - 1][1] = now;
             }
 
-            if (now * 2 <= 100_000 && arr[now*2] == 0) {
-                queue.offer(now*2);
-                arr[now*2] = arr[now] + 1;
-                prev[now*2] = now;
+            if (now * 2 <= 100_000 && arr[now * 2][0] == 0) {
+                queue.offer(now * 2);
+                arr[now * 2][0] = arr[now][0] + 1;
+                arr[now * 2][1] = now;
             }
-
         }
 
-        int index = m;
-        List<Integer> list = new ArrayList<>();
-        StringBuilder builder = new StringBuilder();
+        System.out.println(builder);
 
-        while (index != n) {
-            list.add(index);
-            index = prev[index];
-        }
-
-        Collections.reverse(list);
-
-        builder.append(n).append(" ");
-        list.stream().forEach(num -> builder.append(num).append(" "));
-
-        System.out.print(builder);
-        reader.close();
     }
 }
