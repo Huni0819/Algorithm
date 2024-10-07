d182c5c7c2650e4eb66dc0f3d1fb670e19af654d
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-10-07 13:31:01 +0900

    [Gold III] Title: 다리 만들기, Time: 480 ms, Memory: 297520 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -2,92 +2,93 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static int[][] d = {
+            {1, 0}, {-1, 0}, {0, 1}, {0, -1}
+    };
 
-    static int n;
+    static int N;
     static int[][] arr;
-    static int[][] visited;
 
     public static void main(String[] args) throws Exception {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(reader.readLine());
-        arr = new int[n][n];
+        N = Integer.parseInt(reader.readLine());
 
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(reader.readLine());
+        arr = new int[N][N];
 
-            // 0은 바다, 1은 육지
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+
+            String[] input = reader.readLine().split(" ");
+
+            for (int j = 0; j < N; j++) {
+
+                arr[i][j] = Integer.parseInt(input[j]);
             }
         }
 
         int min = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                // arr[i][j]가 1이어도 상하좌우가 다 육지면 스킵
-                // 바다와 인접해 있는 경우만 검사.
-                if (arr[i][j] == 1 && checkCoast(i, j)) {
-                    min = Math.min(min, bfs(i, j));
-                }
-            }
-        }
-
-        System.out.println(min-1);
-    }
-
-    private static boolean checkCoast(int x, int y) {
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (checkOutOfBound(nx, ny)) {
-                continue;
-            }
-
-            if (arr[nx][ny] == 0) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private static boolean checkOutOfBound(int x, int y) {
-        return x < 0 || x >= n || y < 0 || y >= n;
-    }
-
-    private static boolean checkAnotherIsland(Coord standard, Coord other) {
-        boolean[][] visited = new boolean[n][n];
-
-        Queue<Coord> queue = new LinkedList<>();
-        queue.offer(new Coord(standard.getX(), standard.getY()));
-        visited[standard.getX()][standard.getY()] = true;
-
-        while (!queue.isEmpty()) {
-            Coord now = queue.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = now.getX() + dx[i];
-                int ny = now.getY() + dy[i];
-
-                if (checkOutOfBound(nx, ny)) {
+                if (arr[i][j] == 0) {
                     continue;
                 }
 
-                if (nx == other.getX() && ny == other.getY()) {
+                int count = 0;
+
+                for (int k = 0; k < d.length; k++) {
+
+                    int nx = i + d[k][0];
+                    int ny = j + d[k][1];
+
+                    if (!checkOutOfBound(nx, ny) && arr[nx][ny] == 0) {
+                        count++;
+                    }
+                }
+
+                if (count > 0) {
+                    min = Math.min(min, solution(i, j));
+                }
+            }
+        }
+
+        System.out.println(min);
+    }
+
+    static boolean checkOutOfBound(int x, int y) {
+        return x < 0 || x >= N || y < 0 || y >= N;
+    }
+
+    static boolean checkOtherIsland(int tx, int ty, int ox, int oy) {
+
+        Queue<Coord> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[N][N];
+
+        queue.offer(new Coord(tx, ty));
+        visited[tx][ty] = true;
+
+        while (!queue.isEmpty()) {
+
+            Coord now = queue.poll();
+
+            for (int i = 0; i < d.length; i++) {
+
+                int nx = now.x + d[i][0];
+                int ny = now.y + d[i][1];
+
+                if (checkOutOfBound(nx, ny) || visited[nx][ny]) {
+                    continue;
+                }
+
+                if (nx == ox && ny == oy) {
                     return false;
                 }
 
-                if (!visited[nx][ny] && arr[nx][ny] == 1) {
+                if (arr[nx][ny] == 1) {
                     queue.offer(new Coord(nx, ny));
                     visited[nx][ny] = true;
                 }
@@ -97,32 +98,33 @@ public class Main {
         return true;
     }
 
-    private static int bfs(int x, int y) {
-        visited = new int[n][n];
+    static int solution(int x, int y) {
+
         Queue<Coord> queue = new LinkedList<>();
         queue.offer(new Coord(x, y));
+        int[][] visited = new int[N][N];
         visited[x][y] = 1;
 
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
+
             Coord now = queue.poll();
 
-            for (int i = 0; i < 4; i++) {
-                int nx = now.getX() + dx[i];
-                int ny = now.getY() + dy[i];
+            for (int i = 0; i < d.length; i++) {
+                int nx = now.x + d[i][0];
+                int ny = now.y + d[i][1];
 
-                if (checkOutOfBound(nx, ny)) {
+                if (checkOutOfBound(nx, ny) || visited[nx][ny] != 0) {
                     continue;
                 }
 
-                if (visited[nx][ny] == 0 && arr[nx][ny] == 0) {
-                    queue.offer(new Coord(nx, ny));
-                    visited[nx][ny] = visited[now.getX()][now.getY()] + 1;
-                } else if (visited[nx][ny] == 0 && arr[nx][ny] == 1) {
-                    if (checkAnotherIsland(new Coord(x, y), new Coord(nx, ny))) {
-                        return visited[now.getX()][now.getY()];
-                    }
+                if (arr[nx][ny] == 1 && checkOtherIsland(x, y, nx, ny)) {
+                    return visited[now.x][now.y] - 1;
                 }
 
+                if (arr[nx][ny] == 0) {
+                    queue.offer(new Coord(nx, ny));
+                    visited[nx][ny] = visited[now.x][now.y] + 1;
+                }
             }
         }
 
@@ -132,19 +134,11 @@ public class Main {
 
 class Coord {
 
-    private int x;
-    private int y;
+    int x;
+    int y;
 
     public Coord(int x, int y) {
         this.x = x;
         this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
     }
 }
