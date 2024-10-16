110a552a33b1c1d575d1d56805fa8352f25f87f5
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-10-16 15:29:53 +0900

    [Silver III] Title: 1로 만들기, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
 
@@ -9,46 +7,17 @@ public class Main {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(reader.readLine());
+        int n = Integer.parseInt(reader.readLine());
 
-        System.out.println(solution(N));
+        System.out.println(solution(n, 0));
     }
 
-    static int solution(int n) {
+    static int solution(int n, int count) {
 
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(n);
-        int[] visited = new int[n+1];
-        visited[n] = 1;
-
-        while (!queue.isEmpty()) {
-
-            int now = queue.poll();
-
-            if (now == 1) {
-                return visited[now] - 1;
-            }
-
-            if (now % 3 == 0) {
-                check(now, now/3, visited, queue);
-            }
-
-            if (now % 2 == 0) {
-                check(now, now/2, visited, queue);
-            }
-
-            check(now, now-1, visited, queue);
+        if (n < 2) {
+            return count;
         }
 
-        return visited[1];
+        return Math.min(solution(n/2, count + 1 + (n % 2)), solution(n / 3, count + 1 + (n % 3)));
     }
-
-    static void check(int now, int num, int[] visited, Queue<Integer> queue) {
-
-        if (visited[num] == 0) {
-            queue.offer(num);
-            visited[num] = visited[now] + 1;
-        }
-    }
-
 }
