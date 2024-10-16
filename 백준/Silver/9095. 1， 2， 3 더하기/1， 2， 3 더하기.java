ed4bbbbc1e8fbcbc9bb1107810a98197386463eb
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-10-16 15:39:55 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,58 +1,38 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
 
-    static int[] array = new int[11];
-
     public static void main(String[] args) throws Exception {
 
-        BufferedReader reader =
-                new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter writer =
-                new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder builder = new StringBuilder();
 
-        int t = Integer.parseInt(reader.readLine());
+        int T = Integer.parseInt(reader.readLine());
 
-        for (int i = 0; i < t; i++) {
-            int num = Integer.parseInt(reader.readLine());
+        for (int tc = 0; tc < T; tc++) {
 
-            int result = bottomUp(num);
+            int N = Integer.parseInt(reader.readLine());
 
-            writer.write(Integer.toString(result));
-            writer.write("\n");
-        }
+            int[] dp = new int[N+1];
+            dp[0] = 1;
 
-        writer.flush();
-        reader.close();
-        writer.close();
-    }
-
-    private static int topDown(int num) {
-        array[0] = 1;
-        array[1] = 2;
-        array[2] = 4;
-
-        if (num <= 3 || array[num-1] != 0) {
-            return array[num-1];
-        }
-
-        return array[num-1] = topDown(num-1) + topDown(num-2) + topDown(num-3);
-    }
-
-    private static int bottomUp(int num) {
-        array[0] = 1;
-        array[1] = 2;
-        array[2] = 4;
-
-        if (num > 3) {
-            for (int i = 3; i < num; i++) {
-                array[i] = array[i - 1] + array[i - 2] + array[i - 3];
+            if (N >= 1) {
+                dp[1] = 1;
             }
+
+            if (N >= 2) {
+                dp[2] = 2;
+            }
+
+            for (int i = 3; i <= N; i++) {
+                dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
+            }
+
+            builder.append(dp[N])
+                    .append("\n");
         }
 
-        return array[num-1];
+        System.out.println(builder);
     }
 }
