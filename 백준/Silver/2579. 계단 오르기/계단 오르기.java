828401d6a82fec67a91a5ecdeb484a2517fc2208
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-10-17 15:18:13 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,43 +1,48 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Objects;
 
 public class Main {
 
+    static int N;
     static int[] arr;
-    static int n;
-    static int[] calc;
+    static Integer[] dp;
 
     public static void main(String[] args) throws Exception {
 
-        BufferedReader reader =
-                new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(reader.readLine());
-        arr = new int[n+1];
-        calc = new int[n+1];
+        N = Integer.parseInt(reader.readLine());
 
-        for (int i = 1; i <= n; i++) {
+        arr = new int[N+1];
+        dp = new Integer[N+1];
+
+        for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(reader.readLine());
         }
 
-        calc[1] = arr[1];
+        dp[0] = 0;
 
-        if (n >= 2) {
-            calc[2] = arr[1] + arr[2];
+        if (N >= 1) {
+            dp[1] = arr[1];
         }
 
-        System.out.println(solution(n));
-        reader.close();
+        if (N >= 2) {
+            dp[2] = arr[2] + arr[1];
+        }
+
+        int result = solution(N);
+
+        System.out.println(result);
     }
 
-    private static int solution(int num) {
+    static int solution(int N) {
 
-        for (int i = 3; i <= num; i++) {
-            calc[i] = Math.max(calc[i-2], calc[i-3] + arr[i-1]) + arr[i];
+        if(Objects.isNull(dp[N])) {
+
+            dp[N] = Math.max(solution(N-3) + arr[N-1], solution(N-2)) + arr[N];
         }
 
-        return calc[num];
+        return dp[N];
     }
 }
