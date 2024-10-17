e30a3719e9ddc6e30951e6f2ebd62bd4950eac2f
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-10-17 18:40:40 +0900

    [Silver III] Title: 피보나치 함수, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -3,45 +3,36 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static StringBuilder builder = new StringBuilder();
-    static int[] zeroArr;
-    static int[] oneArr;
-
     public static void main(String[] args) throws Exception {
 
-        BufferedReader reader =
-                new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder builder = new StringBuilder();
 
-        int t = Integer.parseInt(reader.readLine());
+        int T = Integer.parseInt(reader.readLine());
 
-        for (int i = 0; i < t; i++) {
-            int n = Integer.parseInt(reader.readLine());
-            zeroArr = new int[n+1];
-            oneArr = new int[n+1];
-            solution(n);
+        for (int tc = 0; tc < T; tc++) {
+
+            int N = Integer.parseInt(reader.readLine());
+
+            int[][] dp = new int[N+1][2];
+            dp[0][0] = 1;
+
+            if (N >= 1) {
+                dp[1][1] = 1;
+            }
+
+            for (int i = 2; i <= N; i++) {
+
+                dp[i][0] = dp[i-1][0] + dp[i-2][0];
+                dp[i][1] = dp[i-1][1] + dp[i-2][1];
+            }
+
+            builder.append(dp[N][0])
+                    .append(" ")
+                    .append(dp[N][1])
+                    .append("\n");
         }
 
-        System.out.println(builder);
-        reader.close();
-    }
-
-    private static void solution(int num) {
-        if (num == 0) {
-            builder.append(1).append(" ").append(0).append("\n");
-            return;
-        } else if (num == 1) {
-            builder.append(0).append(" ").append(1).append("\n");
-            return;
-        }
-
-        zeroArr[0] = oneArr[1] = 1;
-        zeroArr[1] = oneArr[0] = 0;
-
-        for (int i = 2; i <= num; i++) {
-            zeroArr[i] = zeroArr[i-1] + zeroArr[i-2];
-            oneArr[i] = oneArr[i-1] + oneArr[i-2];
-        }
-
-        builder.append(zeroArr[num]).append(" ").append(oneArr[num]).append("\n");
+        System.out.print(builder);
     }
 }
