99ac45afbaaa64f428957367275982917cfdabab
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-10-17 15:58:04 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -3,34 +3,26 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static long[] arr;
-
     public static void main(String[] args) throws Exception {
 
-        BufferedReader reader =
-                new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(reader.readLine());
+        int N = Integer.parseInt(reader.readLine());
 
-        System.out.println(solution(n));
+        int[] dp = new int[N+1];
 
-        reader.close();
-    }
-
-    private static long solution(int n) {
-        if (n == 1) {
-            return 1;
-        } else if (n == 2) {
-            return 2;
-        }
-        arr = new long[n+1];
-        arr[1] = 1;
-        arr[2] = 2;
-
-        for (int i = 3; i <= n; i++) {
-            arr[i] = (arr[i-1] + arr[i-2]) % 10_007;
+        if (N >= 1) {
+            dp[1] = 1;
         }
 
-        return arr[n];
+        if (N >= 2) {
+            dp[2] = 2;
+        }
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i-2] + dp[i-1]) % 10_007;
+        }
+
+        System.out.println(dp[N]);
     }
 }
