006400981971fb74384084a02e0a63b941684255
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-10-21 12:30:23 +0900

    [Silver III] Title: 파도반 수열, Time: 100 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -4,37 +4,32 @@ import java.io.InputStreamReader;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        BufferedReader reader =
-                new BufferedReader(new InputStreamReader(System.in));
 
-        /**
-         * arr[1] = arr[2] = arr[3] = 1
-         *
-         * arr[4] 부터
-         * arr[n] = arr[n-2] + arr[n-3]
-         */
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int t = Integer.parseInt(reader.readLine());
+        int T = Integer.parseInt(reader.readLine());
 
-        for (int i = 0; i < t; i++) {
-            int n = Integer.parseInt(reader.readLine());
+        for (int tc = 0; tc < T; tc++) {
 
-            if (n <= 3) {
-                System.out.println(1);
-                continue;
+            int N = Integer.parseInt(reader.readLine());
+
+            long[] dp = new long[N+1];
+            dp[1] = 1;
+
+            if (N >= 2) {
+                dp[2] = 1;
             }
 
-            long arr[] = new long[n+1];
-
-            arr[1] = arr[2] = arr[3] = 1;
-
-            for (int j = 4; j <= n; j++) {
-                arr[j] = arr[j-2] + arr[j-3];
+            if (N >= 3) {
+                dp[3] = 1;
             }
 
-            System.out.println(arr[n]);
+            for (int i = 4; i <= N; i++) {
+                dp[i] = dp[i-2] + dp[i-3];
+            }
+
+            System.out.println(dp[N]);
         }
 
-        reader.close();
     }
 }
