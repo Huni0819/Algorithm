dfc606091dc9d392c35cd328cbb07c58f345931b
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-11-06 19:26:20 +0900

    [Silver II] Title: 랜선 자르기, Time: 164 ms, Memory: 17856 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,60 +1,46 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int k;
-    static int n;
-    static int[] arr;
-    static long max;
-    static long min;
-
-
     public static void main(String[] args) throws Exception {
 
-        BufferedReader reader =
-                new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(reader.readLine());
-        k = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
+        String[] input = br.readLine().split(" ");
 
-        arr = new int[k];
-        for (int i = 0; i < k; i++) {
-            arr[i] = Integer.parseInt(reader.readLine());
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
 
-            max = Math.max(arr[i], max);
+        long min = 0;
+        long max = Long.MIN_VALUE;
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, arr[i]);
         }
 
         max++;
 
-        System.out.println(solution());
-        reader.close();
-    }
-
-    private static long solution() {
-
-        if (max == 1) {
-            return 1;
-        }
-
         while (min < max) {
+
             long mid = (min + max) / 2;
+
             int count = 0;
 
-            for (int i = 0; i < k; i++) {
-                count += (arr[i] / mid);
+            for (int i = 0; i < N; i++) {
+                count += arr[i] / mid;
             }
 
-            if (count < n) {
+            if (count < K) {
                 max = mid;
             } else {
                 min = mid + 1;
             }
-
         }
 
-        return min-1;
+        System.out.print(min - 1);
     }
 }
