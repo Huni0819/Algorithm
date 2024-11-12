af246aab043bbad9812d5e68299b052592b7484d
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-11-12 16:25:48 +0900

    [Silver I] Title: 미로 탐색, Time: 120 ms, Memory: 14916 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,79 +1,84 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int[][] d = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    static int[][] d = {
+            { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 }
+    };
 
-    static int n;
-    static int m;
+    static int N;
+    static int M;
 
-    static int[][] arr;
-    static boolean[][] visited;
+    static char[][] arr;
 
     public static void main(String[] args) throws Exception {
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] input = reader.readLine().split(" ");
-        n = Integer.parseInt(input[0]);
-        m = Integer.parseInt(input[1]);
+        String[] input = br.readLine().split(" ");
 
-        arr = new int[n][m];
-        visited = new boolean[n][m];
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
 
-        for (int i = 0; i < n; i++) {
-            String line = reader.readLine();
+        arr = new char[N][M];
 
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(Character.toString(line.charAt(j)));
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = line.charAt(j);
             }
         }
 
-        solution();
+        System.out.println(solution());
     }
 
-    private static void solution() {
-        Queue<Coord> queue = new LinkedList<>();
-        queue.offer(new Coord(0, 0, 1));
-        visited[0][0] = true;
+    static int solution() {
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(0, 0));
 
-        while(!queue.isEmpty()) {
-            Coord now = queue.poll();
+        Integer[][] visited = new Integer[N][M];
+        visited[0][0] = 1;
 
-            for (int i = 0; i < 4; i++) {
+        while (!queue.isEmpty()) {
+
+            Node now = queue.poll();
+
+            if (now.x == N - 1 && now.y == M - 1) {
+                break;
+            }
+
+            for (int i = 0; i < d.length; i++) {
                 int nx = now.x + d[i][0];
                 int ny = now.y + d[i][1];
 
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny]) {
+                if (isOutOfBound(nx, ny) || Objects.nonNull(visited[nx][ny])) {
                     continue;
                 }
 
-                if (nx == n-1 && ny == m-1) {
-                    System.out.println(now.count+1);
-                    System.exit(0);
-                }
-
-                if (arr[nx][ny] == 1) {
-                    visited[nx][ny] = true;
-                    queue.offer(new Coord(nx, ny, now.count + 1));
+                if (arr[nx][ny] == '1') {
+                    queue.offer(new Node(nx, ny));
+                    visited[nx][ny] = visited[now.x][now.y] + 1;
                 }
             }
         }
+
+        return visited[N - 1][M - 1];
+    }
+
+    static boolean isOutOfBound(int x, int y) {
+        return x < 0 || x >= N || y < 0 || y >= M;
     }
 }
 
-class Coord {
+class Node {
 
     int x;
     int y;
-    int count;
 
-    public Coord(int x, int y, int count) {
+    public Node(int x, int y) {
         this.x = x;
         this.y = y;
-        this.count = count;
     }
 }
