586b6d5a4e93dd75727ee4127e03f4266a072dc8
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-11-13 15:08:46 +0900

    [Silver III] Title: 바이러스, Time: 112 ms, Memory: 14548 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,61 +1,65 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Main {
 
-    static int count = 0;
-    static int n;
+    static int V;
+    static int E;
+
     static List<Integer>[] list;
-    static boolean[] visited;
 
     public static void main(String[] args) throws Exception {
 
-        BufferedReader reader =
-                new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(reader.readLine());
-        int m = Integer.parseInt(reader.readLine());
+        V = Integer.parseInt(br.readLine());
+        E = Integer.parseInt(br.readLine());
 
-        list = new LinkedList[n+1];
-        visited = new boolean[n+1];
+        list = new List[V + 1];
 
-        for (int i = 0; i <= n; i++) {
-            list[i] = new LinkedList<>();
+        for (int i = 1; i <= V; i++) {
+            list[i] = new ArrayList<>();
         }
 
-        StringTokenizer st;
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(reader.readLine());
+        for (int i = 0; i < E; i++) {
 
-            int one = Integer.parseInt(st.nextToken());
-            int two = Integer.parseInt(st.nextToken());
+            int[] u = Stream.of(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
 
-            list[one].add(two);
-            list[two].add(one);
+            list[u[0]].add(u[1]);
+            list[u[1]].add(u[0]);
         }
 
-        solution(1);
-
-        System.out.println(count);
-        reader.close();
+        System.out.print(solution());
     }
 
-    private static void solution(int num) {
+    static int solution() {
+
         Queue<Integer> queue = new LinkedList<>();
-        visited[num] = true;
-        queue.offer(num);
+        boolean[] visited = new boolean[V + 1];
+
+        int count = 0;
+        visited[1] = true;
+        queue.offer(1);
 
         while (!queue.isEmpty()) {
+
             int now = queue.poll();
 
-            for (int i = 0; i < list[now].size(); i++) {
-                if (!visited[list[now].get(i)]) {
-                    queue.offer(list[now].get(i));
-                    visited[list[now].get(i)] = true;
-                    count++;
+            for (int next : list[now]) {
+
+                if (visited[next]) {
+                    continue;
                 }
+
+                queue.offer(next);
+                visited[next] = true;
+                count++;
             }
         }
+
+        return count;
     }
 }
