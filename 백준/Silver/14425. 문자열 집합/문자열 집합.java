9d1e1344d0679a0932cd23011f39b9f5fa8c4e93
hun_e <122001936+Huni0819@users.noreply.github.com>
2025-05-04 23:07:57 +0900

    [Silver IV] Title: 문자열 집합, Time: 2840 ms, Memory: 30672 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+
+            list.add(br.readLine());
+        }
+
+        int count = 0;
+        for (int i = 0; i < M; i++) {
+
+            String word = br.readLine();
+
+            if (list.contains(word)) {
+
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
