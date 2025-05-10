e8cd74f356efe5054683b561fa49ba5ebb264fd4
hun_e <122001936+Huni0819@users.noreply.github.com>
2025-05-10 21:19:38 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 124 ms, Memory: 15952 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        int c = Integer.parseInt(input[2]);
+        int d = Integer.parseInt(input[3]);
+        int e = Integer.parseInt(input[4]);
+        int f = Integer.parseInt(input[5]);
+
+        int x = (c * e - b * f) / (a * e - b * d);
+        int y = (c * d - a * f) / (b * d - a * e);
+
+        System.out.println(x + " " + y);
+    }
+}
